49315d1bbd1ae1614ed83d4b96b50b29a7469dfd
SonOfMagic <qq1324318532@gmail.com>
2022-02-28 17:57:05 +0800

    feat: commit codes

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,23 @@
 import path from 'path'
-import { createServer } from 'vite'
+import { createServer, build } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-;(async () => {
+import type { RollupOutput } from 'rollup'
+
+export async function createDevServer () {
   const server = await createServer({
     plugins: [vue(), vueJsx()],
     // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
     configFile: false,
-    root: path.resolve(__dirname, 'vite'),
+    root: process.cwd(),
     server: {
-      port: 9000
+      port: 9080
     },
     build: {
       lib: {
         entry: path.resolve(__dirname, 'src/components/HelloWorld.vue'),
         name: 'MyLib',
         fileName: (format) => `my-lib.${format}.js`
-        // formats:['umd','cjs','es']
       },
       rollupOptions: {
         // 确保外部化处理那些你不想打包进库的依赖
@@ -33,4 +34,34 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
   await server.listen()
 
   server.printUrls()
-})()
+}
+
+export async function buildLibrary (entry: string) {
+  const output = (await build({
+    plugins: [vue(), vueJsx()],
+    // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
+    configFile: false,
+    root: process.cwd(),
+    server: {
+      port: 9080
+    },
+    build: {
+      lib: {
+        entry,
+        name: 'lib',
+        fileName: (format) => `lib.${format}.js`
+      },
+      rollupOptions: {
+        // 确保外部化处理那些你不想打包进库的依赖
+        external: ['vue'],
+        output: {
+          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: {
+            vue: 'Vue'
+          }
+        }
+      }
+    }
+  })) as RollupOutput
+  return output
+}
