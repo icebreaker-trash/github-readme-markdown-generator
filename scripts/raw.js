322b63b63ec9b1d7d53ae194fca90879765e47c7
SonOfMagic <qq1324318532@gmail.com>
2022-02-28 16:41:11 +0800

    feat: add commit codes

--- scripts/raw.js
+++
@@ -1,8 +0,0 @@
-const { vue2MdByPath } = require('../')
-// const fsp = require('fs/promises')
-const path = require('path')
-;(async () => {
-  const filepath = path.resolve(__dirname, '../demos/vue/index.vue')
-  const result = await vue2MdByPath(filepath)
-  return result
-})()
--- src/index.ts
+++ src/index.ts
@@ -1,117 +1,36 @@
-// import juice from 'juice'
-import fs from 'fs/promises'
-import { createSSRApp } from 'vue'
-import { createServer } from 'vite'
-import { renderToString } from 'vue/server-renderer'
-import { Module } from 'module'
 import path from 'path'
-import {
-  parse,
-  // compileStyleAsync,
-  compileScript,
-  compileTemplate,
-  compileStyleAsync,
-  compileStyle,
-  rewriteDefault
-  // transformRef,
-  // shouldTransform
-} from 'vue/compiler-sfc'
-require = require('esm')(module/*, options */)
-export function createApp () {
-  return createSSRApp({
-    data: () => ({ count: 1 }),
-    template: '<div @click="count++">{{ count }}</div>'
-  })
-}
-const parentModule = module
-function execute (code: string) {
-  const resource = 'test.js'
-  const module = new Module(resource, parentModule)
-  // eslint-disable-next-line no-underscore-dangle
-  // @ts-ignore
-  module.paths = Module._nodeModulePaths(path.resolve(__dirname, './fixtures'))
-  module.filename = resource
-
-  // eslint-disable-next-line no-underscore-dangle
-  // @ts-ignore
-  module._compile(
-    code,
-    resource
-  )
-
-  return module.exports
-}
-
-export async function vue2Md (source: string) {
-  const id = 'som'
-  const dataVId = 'data-v-' + id
-  const { descriptor } = parse(source, { sourceMap: false })
-  const hasScoped = descriptor.styles.some((s) => s.scoped)
-  const template = compileTemplate({
-    filename: descriptor.filename,
-    id,
-    source: descriptor.template?.content as string,
-    scoped: hasScoped,
-    compilerOptions: {
-      scopeId: hasScoped ? dataVId : undefined
-    }
-  })
-  const script = compileScript(descriptor, {
-    id,
-    templateOptions: {
-      scoped: hasScoped,
-      compilerOptions: {
-        scopeId: hasScoped ? dataVId : undefined
+import { createServer } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+;(async () => {
+  const server = await createServer({
+    plugins: [vue(), vueJsx()],
+    // 任何合法的用户配置选项，加上 `mode` 和 `configFile`
+    configFile: false,
+    root: path.resolve(__dirname, 'vite'),
+    server: {
+      port: 9000
+    },
+    build: {
+      lib: {
+        entry: path.resolve(__dirname, 'src/components/HelloWorld.vue'),
+        name: 'MyLib',
+        fileName: (format) => `my-lib.${format}.js`
+        // formats:['umd','cjs','es']
+      },
+      rollupOptions: {
+        // 确保外部化处理那些你不想打包进库的依赖
+        external: ['vue'],
+        output: {
+          // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+          globals: {
+            vue: 'Vue'
+          }
+        }
       }
     }
   })
-  const styles = descriptor.styles
-  const styleCodes = []
+  await server.listen()
 
-  if (styles.length) {
-    for (let i = 0; i < styles.length; i++) {
-      const styleItem = styles[i]
-      styleCodes.push(
-        compileStyle({
-          filename: descriptor.filename,
-          source: styleItem.content,
-          id: dataVId,
-          scoped: styleItem.scoped
-        }).code
-      )
-    }
-  }
-  const styleCode = styleCodes.join('\n')
-  const renderName = '_sfc_render'
-  const mainName = '_sfc_main'
-  const templateCode = template.code.replace(
-    /\nexport (function|const) (render|ssrRender)/,
-    '\n$1 _sfc_$2'
-  )
-  const scriptCode = rewriteDefault(script.content, mainName)
-
-  const output = [
-    scriptCode,
-    templateCode,
-
-    mainName + '.render=' + renderName,
-    'export default ' + mainName
-  ]
-  if (hasScoped) {
-    output.push(mainName + '.__scopeId = ' + JSON.stringify(dataVId))
-  }
-  const code = output.join('\n')
-  console.log(code)
-  const res = execute(code)
-  console.log(res)
-  await fs.writeFile('./demo.js', code, {
-    encoding: 'utf-8'
-  })
-}
-
-export async function vue2MdByPath (filepath: string) {
-  const source = await fs.readFile(filepath, {
-    encoding: 'utf-8'
-  })
-  return await vue2Md(source)
-}
+  server.printUrls()
+})()
